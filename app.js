56d51ac2d1cdfa8c93cd2de197527e47545a340e
Joao Vitor <jvcr1501@gmail.com>
2024-10-16 23:49:11 -0300

    tenta resolver problema com diretório público

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("#downloadBtn").click(function() {
         const link = document.createElement("a");
         link.href = "/qr_img.png";
-        link.download = "qr_img.png"; // Nome do arquivo a ser baixado
+        link.download = "qr_img.png"; 
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
